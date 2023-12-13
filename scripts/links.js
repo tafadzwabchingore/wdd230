136473f27ea6df3c74fa1994ebc14021df0feb6e
tafadzwabchingore <tafadzwabchingore@gmail.com>
2023-12-13 02:55:21 -0700

    commit

---
+++ scripts/links.js
@@ -0,0 +1,16 @@
+const baseURL = "https://tafadzwabchingore.github.io/wdd230/";
+
+const linksURL = "https://tafadzwabchingore.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+    //console.log(data);
+}
+
+getLinks();
+
+function displayLinks(weeks) {
+    // Code to be generated
+}
